b2bfd92b9cb88d0526d6e89937d88e1c66bbb0ca
Alex <73224325+madebomb25@users.noreply.github.com>
2024-11-17 14:38:52 +0100

    Add files via upload

--- main.cc
+++ main.cc
@@ -14,11 +14,60 @@ CharMatrix gen_board(unsigned int rows, unsigned int columns)
     return CharMatrix(rows, Row(columns, '.'));
 }
 
+// Imprime las opciones que el jugador tiene y devuelve la opción que haya seleccionado
+// el jugador.
+// Pre: haber impreso el tablero del jugador anteriormente.
+// Post: devuelve un char con la opción seleccionada.
+char ask_option()
+{
+    char option;
+    cin >> option;
+
+    ///////////////////////////////////////////////////////////////////
+    // Hacer código para mostrar las opciones disponibles y que hacen.
+    //////////////////////////////////////////////////////////////////
+
+    if (option == 'A')
+    {
+
+    }
+    else if (option == 'B')
+    {
+
+    }
+    else if (option == 'E')
+    {
+
+    }
+    else if (option == 'F')
+    {
+
+    }
+    else if (option == 'R')
+    {
+
+    }
+    else if (option == 'S')
+    {
+
+    }
+    else if (option == 'Z')
+    {
+
+    }
+}
+
 void print_board(CharMatrix board)
 {
     for (unsigned int y=0; y < board.size(); ++y)
     {
+        for (unsigned int x=0; x < board[0].size(); ++x)
+        {
+            cout << board[y][x];
+            cout << " ";
+        }
 
+        cout << endl;
     }
 }
 
@@ -26,27 +75,26 @@ int main()
 {
     ////////// LECTURA DE DATOS INICIAL //////////
 
-vector<int> extraerelementos(const vector<int>& entrada) {
-    vector<int> pistas;
-
-    int n = entrada[0] 
-    if (n > 0 and n <= (entrada.size() - 1)) {
-        for (int i = 1; i <= n; ++i) {
-            pistas.push_back(entrada[i]);
-        }
-    }
-
-    return pistas;
-}
 
     //////////// LOGICA DEL JUEGO ///////////////
 
     bool player_won = false;
 
+    //Generamos el tablero inicial que el jugador tendrá.
+    //(por ahora para hacer pruebas me los invento)
+    CharMatrix player_board = gen_board(10, 10);
+
     // Mientras el jugador no haya ganado seguiremos ejecutando el juego.
     while(!player_won)
     {
-        //Inv: 
+        // Inv: seguimos actualizando el estado del juego, ya que el jugador
+        // aún no ha ganado.
+
+        print_board(player_board);
+
+        // Después de imprimir el tablero le preguntamos al jugador que quiere
+        // hacer.
+        char option = ask_option();
     }
 
     //////// HACER EN EL FIN DEL JUEGO //////
