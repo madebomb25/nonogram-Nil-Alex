7587c3726612f687f0a8f36a92789718ff45f1d7
Alex <73224325+madebomb25@users.noreply.github.com>
2024-12-31 22:47:54 +0100

    Add files via upload

--- main.cc
+++ main.cc
@@ -28,7 +28,7 @@ unsigned int count_digits(unsigned int n)
 // Encuentra el largo del vector de pistas más grande de una matriz de pistas UIntMatrix.
 // Pre: una matriz CharMatrix que contenga los vectores de pistas a comparar.
 // Pos: devuelve un número natural que indica el tamaño del vector de pistas más grande encontrado.
-unsigned int find_biggest_clue_size(UIntMatrix clue_matrix)
+unsigned int find_biggest_clue_size(const UIntMatrix &clue_matrix)
 {
    // Asumimos por defecto que el vector más grande es el primero.
    unsigned int biggest_clue_vector_size = clue_matrix[0].size();
@@ -73,24 +73,19 @@ UIntMatrix built_clues(unsigned int clue_package_size)
 
 // __________________FUNCIONES PARA LA DE GESTIÓN DEL TABLERO ___________________
 
-// Imprime las líneas horizontales que separan las leyendas horizontales de las columnas del tablero
-// con el tablero en si.
+// Genera la línea de '-'s que cierra la parte superior e inferior del tablero.
 
 // Pre: 'board_size_x' representa el número natural que nos indica el largo del tablero, mientras
 // que 'board_size_y' es el número natural que aporta la altura que ayudará a poner los espacios
 // en blanco a la izquierda de la fila.
 
-// Post: no devuelve nada, solo imprime una línea de guiones en base al largo del tablero.
-void print_horizontal_lines(unsigned int board_size_x, unsigned int board_size_y)
+// Post: devuelve un string con la línea ya formateada y preparada para su impresión directa.
+string gen_horizontal_line(unsigned int board_size_x, unsigned int board_size_y)
 {
+   string slash_line = "";
 
    // Añadimos el espacio en blanco que separa las líneas de la leyenda de las filas.
-   cout << string(count_digits(board_size_y), ' ');
-
-   // Con 'board_size_x - 1' obtenemos la cantidad de veces que habrá que poner 2 espacios en
-   // blanco entre la leyenda de cada columna. Luego lo multiplicamos por 2 para obtener 2 espacios
-   // por leyenda.
-   unsigned int space_slashes = (board_size_x - 1) * 2;
+   slash_line += string(count_digits(board_size_y), ' ');
 
    // Sumamos las longitudes de todos los números para encontrar la cantidad de '-' que necesitaremos
    // poner debajo de ellos.
@@ -105,13 +100,15 @@ void print_horizontal_lines(unsigned int board_size_x, unsigned int board_size_y
    // Imprimimos la línea entera ya sabiendo la cantidad de '-' que necesitaremos tanto
    // para los dobles espacios en blanco para cada número de la leyenda y la cantidad de '-'
    // que van debajo de cada número, lo que nos da la línea entera.
-   cout << string(total_sum_num_length + (board_size_x - 1) * 2, '-') << endl;
+   slash_line += string(total_sum_num_length + (board_size_x - 1) * 2, '-');
+
+   return slash_line;
 }
 
 // Imprime la parte superior del tablero.
 // Pre:
 // Post:
-void print_board_top(unsigned int board_size_x, unsigned int board_size_y)
+void print_top_legend(unsigned int board_size_x, unsigned int board_size_y)
 {
    // Restamos 1 por que ya hemos añadido un espacio con el primer elemento de la leyenda.
    cout << string(count_digits(board_size_y) - 1, '-');
@@ -121,8 +118,6 @@ void print_board_top(unsigned int board_size_x, unsigned int board_size_y)
       cout << " " << i << " ";
 
    cout << endl;
-
-   print_horizontal_lines(board_size_x, board_size_y);
 }
 
 // Imprime la parte inferior del tablero la cual contiene las pistas para las columnas.
@@ -130,10 +125,8 @@ void print_board_top(unsigned int board_size_x, unsigned int board_size_y)
 // el alto (board_size_y) y finalmente una matriz de naturales que contenga las pistas de las columnas
 // (col_clues).
 // Post: no devuelve nada, solo imprime la parte inferior del tablero.
-void print_board_bottom(unsigned int board_size_x, unsigned int board_size_y, UIntMatrix &col_clues)
+void print_bottom_clues(unsigned int board_size_x, unsigned int board_size_y, const UIntMatrix &col_clues)
 {
-   print_horizontal_lines(board_size_x, board_size_y);
-
    for (unsigned int i = 0; i < find_biggest_clue_size(col_clues); ++i)
    {
       // Inv: hemos impreso 'i' líneas de pistas para las pistas de las columnas.
@@ -154,21 +147,18 @@ void print_board_bottom(unsigned int board_size_x, unsigned int board_size_y, UI
          else
             cout << " ";
       }
-
       cout << endl;
    }
 }
 
-// Imprime el estado actual de la partida.
+// Imprime el estado actual de la partida. No incluye los elementos decorativos como las pistas.
+//
 // Pre: la matriz de tipo CharMatrix que representa el tablero y las matrices tipo UIntMatrix que representan
 // las pistas de las filas y de las columnas.
+//
 // Post: no devuelve nada.
-void print_board(CharMatrix &board, UIntMatrix &row_clues, UIntMatrix &col_clues)
+void print_core_board(CharMatrix &board, const UIntMatrix &row_clues, const UIntMatrix &col_clues)
 {
-
-   // Imprimimos la leyenda para las columnas y las líneas que cierran la cara superior
-   // del tablero.
-   print_board_top(board[0].size(), board.size());
 
    //// Impresión del tablero principal ////
 
@@ -192,19 +182,174 @@ void print_board(CharMatrix &board, UIntMatrix &row_clues, UIntMatrix &col_clues
       for (unsigned int i = 0; i < row_clues[y].size(); ++i)
          cout << ' ' << row_clues[y][i];
    }
-
-   // Imprimimos las líneas que cierran la cara inferior del tablero y imprimimos
-   // las pistas para las columnas.
-   print_board_bottom(board[0].size(), board.size(), col_clues);
 }
+
+void draw_game(const UIntMatrix &row_clues, const UIntMatrix &col_clues, CharMatrix &board, string slash_line)
+{
+   unsigned int board_size_x = board[0].size();
+   unsigned int board_size_y = board.size();
+
+   print_top_legend(board_size_x, board_size_y); // Imprimimos la leyenda de las columnas.
+   cout << slash_line << endl;                   // Imprimimos las '-'s que cierran techo del tablero.
+
+   print_core_board(board, row_clues, col_clues); // Imprimimos el tablero.
+
+   cout << slash_line << endl; // Imprimimos las '-'s que cierran el suelo del tablero.
+
+   print_bottom_clues(board_size_x, board_size_y, col_clues);
+}
+
+// ________ FUNCIONES DE ACCIÓN _________
+
+// Se marca o desmarca la casilla en las coordenadas X (selected_x) e Y (selected_y) en el tablero (board). Dependiendo de la opción,
+// no haremos nada en los casos que ya estén marcadas/desmarcadas.
+//
+// Pre: dos números naturales (para las coordenadas en selected_x e selected_y) y una matriz de caracteres
+// a modificar (el tablero). Finalmente, un boleano 'check' que nos indica si deseamos marcar (True) o
+// desmarcar (False).
+//
+// Pos: la función no devuelve nada, pero si modifica la variable 'board' para editar el tablero que esta
+// contiene.
+void do_action_AE(bool check, unsigned int selected_x, unsigned int selected_y, CharMatrix &board)
+{
+   if (check and board[selected_y][selected_x] == '.')
+      board[selected_y][selected_x] = 'X';
+   else if (!check and board[selected_y][selected_x] == 'X')
+      board[selected_y][selected_x] = '.';
+}
+
+// Se marcan o desmarcan todas las casillas dentro del rectangulo que delimitan las coordenadas X e Y indicadas dependiendo
+// de si desea marcar o desmarcar. Además, dependiendo de la opción, no se modificarán aquellas casillas que
+// ya estuvieran marcadas/desmarcadas.
+//
+// Pre: 4 números naturales que indiquen las coordenadas X de inicio, X final, Y inicio e Y final y una matriz
+// CharMatrix que contenga el tablero a modificar. Finalmente, un boleano que nos indica si queremos marcar casillas
+// (true) o desmarcarlas (false).
+//
+// Pos: no devuelve nada, pero modifica el grupo de casillas dentro del rectangulo que dibujan las coordenadas
+// en el tablero 'board'.
+void do_action_BF(bool check, unsigned int x_start, unsigned int x_end,
+                  unsigned int y_start, unsigned int y_end, CharMatrix &board)
+{
+   for (unsigned int y = y_start; y <= y_end; ++y)
+   {
+      // Inv: hemos marcado las casillas de 'y - y_end' filas del rectangulo.
+
+      for (unsigned int x = x_start; x <= x_end; ++x)
+      {
+         // Inv: hemos marcado 'x - x_start' casillas de la fila 'y' del rectangulo.
+
+         if (check and board[y][x] == '.')
+            board[y][x] = 'X';
+         else if (!check and board[y][x] == 'X')
+            board[y][x] = '.';
+      }
+   }
+}
+
+// Devuelve el contador de acciones realizadas a 0 y reinicia el tablero.
+//
+// Pre: una variable que contenga de tipo número natural cuyo contenido sea la cantidad de acciones
+// hechas (action_counter) y una variable de tipo CharMatrix cuyo contenido sea el tablero que
+// deseamos reiniciar.
+//
+// Pos: no devuelve nada, pero pone la variable 'action_counter' a 0 y rellena todo el tablero (board)
+// con '.'s, devolviendolo a su estado inicial.
+void do_action_R(unsigned int &action_counter, CharMatrix &board)
+{
+   action_counter = 0;
+
+   for (unsigned int y = 0; y < board.size(); ++y)
+   {
+      // Inv: hemos llenado 'y' filas con '.'s.
+      for (unsigned int x = 0; x < board[0].size(); ++x)
+      {
+         // Inv: hemos reemplazado 'x' elementos de la fila 'y' con '.'s.
+
+         if (board[y][x] == 'X')
+            board[y][x] = '.';
+      }
+   }
+}
+
+// Las acciones S y Z están implementadas directamente sobre main().
 
 int main()
 {
+   // ______ Inicialización de variables _______
+
    unsigned int row_clue_boxes, col_clue_boxes;
    cin >> row_clue_boxes >> col_clue_boxes;
+
+   unsigned int action_counter = 0;
 
    CharMatrix board(row_clue_boxes, vector<char>(col_clue_boxes, '.'));
 
    UIntMatrix row_clues = built_clues(row_clue_boxes);
    UIntMatrix col_clues = built_clues(col_clue_boxes);
+
+   string slash_line = gen_horizontal_line(board[0].size(), board.size());
+
+   // _________ Impresión de la partida ___________
+
+   draw_game(row_clues, col_clues, board, slash_line);
+
+   bool win = false; // Para determinar si el jugador ha ganado o ha salido forzadamente.
+   bool end_game = false;
+
+   while (!end_game)
+   {
+      // Inv: el jugador no ha ganado el juego y tampoco ha decidido rendirse.
+
+      // ________ Entrada de opción ________
+
+      char option_letter;
+      cin >> option_letter;
+
+      if (option_letter == 'A' or option_letter == 'E')
+      {
+         unsigned int x, y;
+         cin >> x >> y;
+
+         // Comprobamos si la opción era la A o la E. Ya que si era la A, siempre ibamos a pasar
+         // como parámetro en 'check' un boleano True. En caso de E, siempre es False.
+         bool check = (option_letter == 'A'); 
+
+         do_action_AE(check, x, y, board);
+
+      } 
+      
+      else if (option_letter == 'B' or option_letter == 'F')
+      {
+         unsigned int x_start, x_end, y_start, y_end;
+         cin >> x_start >> y_start >> x_end >> y_end;
+
+         // Comprobamos si la opción era la B o la F. Ya que si era la B, siempre ibamos a pasar
+         // como parámetro en 'check' un boleano True. En caso de F, siempre es False.
+         bool check = (option_letter == 'B');
+
+         do_action_BF(check, x_start, x_end, y_start, y_end, board);
+      }
+
+      else if (option_letter == 'R')
+      {
+         do_action_R(action_counter, board);
+      }
+
+      else if (option_letter == 'S')
+      {
+         draw_game(row_clues, col_clues, board, slash_line);
+      }
+      else
+      {
+         // Como todos los inputs son correctos (según la documentación), tenemos garantizado que
+         // si no se ha ejecutado cualquiera de las anteriores acciones es por que el jugador ha
+         // introducido una 'Z', por lo que salimos del juego.
+
+         end_game = true;
+      }
+   }
+
+   if (win) cout << "Enhorabona! Has resolt el nonograma.";
+   else "Has sortit del joc. Fins aviat!";
 }
